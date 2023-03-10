3289bfb732f9778921221acdbf46b4d00bb0e75b
VirroHarrou <81008781+VirroHarrou@users.noreply.github.com>
2023-03-10 12:46:38 +0300

    Добавьте файлы проекта.

---
+++ Messanger API/Core/Context/MessangerContext.cs
@@ -0,0 +1,27 @@
+﻿using Messanger_API.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messanger_API.Core.Context
+{
+    public class MessangerContext : DbContext
+    {
+        public MessangerContext() 
+        {
+           
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<Like> Likes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True");
+        }
+    }
+}
---
+++ Messanger API/Core/Models/News.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Messanger_API.Core.Models
+{
+    public class News
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Topic { get; set; }
+        [Required]
+        public string MainText { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        public DateTime CreatedAt => DateTime.Now;
+        public DateTime? UpdatedAt { get; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+    }
+}
