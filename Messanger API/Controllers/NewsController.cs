afed3ebaa8e129512a16ba26ccc658e98afb2aa9
VirroHarrou <81008781+VirroHarrou@users.noreply.github.com>
2023-03-27 17:28:11 +0300

    Created full NewsController

--- Messanger API/Controllers/NewsController.cs
+++ Messanger API/Controllers/NewsController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Core.News_.Commands.CreateNews;
+using SocialNetwork.Core.News_.Commands.CreateNews.UpdateNews;
+using SocialNetwork.Core.News_.Commands.DeleteNews;
 using SocialNetwork.Core.News_.Queries.GetNewsDetails;
 using SocialNetwork.Core.News_.Queries.GetNewsList;
+using SocialNetwork.Domain.WorkModels;
 
 namespace SocialNetwork.Controllers
 {
+    [Route("api/[controller]")]
     public class NewsController : BaseController
     {
+        private readonly IMapper _mapper;
+        public NewsController(IMapper mapper) => 
+            _mapper = mapper;
+
         [HttpGet]
         public async Task<ActionResult<NewsListVm>> GetAll(int count, int? skip)
         {
@@ -27,6 +37,36 @@ namespace SocialNetwork.Controllers
             };
             var vm = await Mediator.Send(query);
             return Ok(vm);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create([FromBody] CreateNewsDto createNewsDto)
+        {
+            var command = _mapper.Map<CreateNewsCommand>(createNewsDto);
+            command.UserId = UserId;
+            var noteId = await Mediator.Send(command);
+            return Ok(noteId);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateNewsDto updateNewsDto)
+        {
+            var command = _mapper.Map<UpdateNewsCommand>(updateNewsDto);
+            command.UserId = UserId;
+            await Mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var command = new DeleteNewsCommand
+            {
+                Id = id,
+                UserId = UserId
+            };
+            await Mediator.Send(command);
+            return NoContent();
         }
     }
 }
---
+++ Messanger API/Domain/WorkModels/CreateNewsDto.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using SocialNetwork.Common.Mappings;
+using SocialNetwork.Core.News_.Commands.CreateNews;
+
+namespace SocialNetwork.Domain.WorkModels
+{
+    public class CreateNewsDto : IMapWith<CreateNewsDto>
+    {
+        public string Topic { get; set; }
+        public string MainText { get; set; }
+        public string ImageUrl { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreateNewsDto, CreateNewsCommand>()
+                .ForMember(newsCommand => newsCommand.Topic,
+                opt => opt.MapFrom(newsDto => newsDto.Topic))
+                .ForMember(newsCommand => newsCommand.MainText,
+                opt => opt.MapFrom(newsDto => newsDto.MainText))
+                .ForMember(newsCommand => newsCommand.ImageUrl,
+                opt => opt.MapFrom(newsDto => newsDto.ImageUrl));
+        }
+    }
+}
